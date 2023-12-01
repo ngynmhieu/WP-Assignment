fc3e1d3bb3ecd32650e6f512547fda22c72b9fc6
Quoc Hoan <hoan.tran._.ok@hcmut.edu.vn>
2023-12-01 11:47:35 +0700

    remove personal dropdown

--- home.js
+++ home.js
@@ -1,17 +1,7 @@
-function displayUser(){
-    document.getElementsByClassName("dropdown")[0].classList.toggle("show");
-}
-
 window.onclick = function(event) {
-    // hide the user profile if click out of it
-    if (!event.target.matches(".user")) {
-        console.log(event.target);
-        let dropdowns = document.getElementsByClassName("dropdown");
-        let openDropdown = dropdowns[0];
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-        }
-    }
+    // log target for debug
+    // console.log(event.target);
+
     // confirm if delete resume
     if (event.target.matches(".delete-button")){
         let result = confirm("Are you sure you want to delete this resume ?");
