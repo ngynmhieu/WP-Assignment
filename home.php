070b811295fdb7600413a7576008cc0fb5df3c96
ngynmhieu <ngyn.mhieu@gmail.com>
2023-12-03 17:57:42 +0700

    update

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CV Creator</title>
+    <title>Resume.io</title>
     <link rel="stylesheet" href="home.css">
     
 </head>
