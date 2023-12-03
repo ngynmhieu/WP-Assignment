d1085fbae2078eca22c5aec23a20fdba215cde65
ngynmhieu <ngyn.mhieu@gmail.com>
2023-12-03 17:56:46 +0700

    Update ten cho trang web

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CV Creator</title>
     <link rel="stylesheet" href="home.css">
     
 </head>
