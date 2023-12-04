935eb3a3cc78a11e2a2adab884cb6d7203a56773
ngynmhieu <ngyn.mhieu@gmail.com>
2023-12-04 13:35:50 +0700

    update cv_create.php

--- cv_create.php
+++ cv_create.php
@@ -144,6 +144,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){ //tao cv moi
         }
 }
 $message = "Your CV is successfully created";
-echo "<script type = 'text/javascript'>alert('$message')</script>";
-header ('Location: home.php');
+echo "<script type ='text/javascript'>alert('$message');</script>";
+header ("refresh:0 ; url = home.php");
 ?>
